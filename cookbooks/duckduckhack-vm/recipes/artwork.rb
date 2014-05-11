f924205e7863b6cbe1467ff2ad5c5f8da4165c3d
Mike DePaulo <mikedep333@gmail.com>
2014-05-11 10:42:52 -0400

    Got the XFCE panel & desktop settings working.

--- cookbooks/duckduckhack-vm/recipes/artwork.rb
+++ cookbooks/duckduckhack-vm/recipes/artwork.rb
@@ -27,46 +27,24 @@ end
 
 # Originally I intended to make calls to xfconf-query instead of manaully saving
 # config files. However, xfconf-query refuses to launch without an X session.
+#
 # Set start menu appearance
 # Set background image
 # center the background image
 # Put grey around the (transparent) background image
 # customize icons
-
-directory "/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/" do
-  owner "root"
-  group "root"
-  mode 00755
-  action :create
-  recursive true
-end
-directory "/home/vagrant/.config/xfce4/xfconf/xfce-perchannel-xml" do
-  owner "root"
-  group "root"
-  mode 00755
-  action :create
-  recursive true
-end
-
-cookbook_file "/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml" do
-  source "home-dot-config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml"
+#
+# These dirs will already exist once the packages are installed.
+cookbook_file "/etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml" do
+  source "etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml"
   action :create
 end
-cookbook_file "/home/vagrant/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml" do
-  source "home-dot-config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml"
+cookbook_file "/etc/xdg/xfce4/panel/default.xml" do
+  source "etc/xdg/xfce4/panel/default.xml"
   action :create
 end
 
-cookbook_file "/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml" do
-  source "home-dot-config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml"
-  action :create
-end
-cookbook_file "/home/vagrant/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml" do
-  source "home-dot-config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml"
-  action :create
-end
-
-#customize login screen (lightdm-gtk-greeter)
+# customize login screen (lightdm-gtk-greeter)
 execute "lightdm-gtk-greeter background color" do
   command "sudo sed -i 's/background=.*/background=#ADB5BE/' /etc/lightdm/lightdm-gtk-greeter.conf"
   action :run
