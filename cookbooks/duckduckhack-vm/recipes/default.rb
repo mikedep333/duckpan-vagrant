c7b313e668f534b03f33ccb67c215bb81865bd4d
Mike DePaulo <mikedep333@gmail.com>
2014-07-29 07:59:32 -0400

    Code simplification

--- cookbooks/duckduckhack-vm/recipes/default.rb
+++ cookbooks/duckduckhack-vm/recipes/default.rb
@@ -6,63 +6,39 @@
 include_recipe "duckduckhack-vm::setpassword"
 include_recipe "sublime-text-editor::default"
 
-package "xfce4" do
-  action :install
-end
+package "xfce4"
 
 # xterm & uxterm will also be installed by default
-package "xfce4-terminal" do
-  action :install
-end
+package "xfce4-terminal"
 
 # Enables the Thunar file manager to have icons for files and folders.
-package "xubuntu-icon-theme" do
-  action :install
-end
+package "xubuntu-icon-theme"
 
-package "firefox" do
-  action :install
-end
+package "firefox"
 
 # Commonly used text editors
 # The default package "vim-tiny" has very few features compared to regular vim
-package "vim" do
-  action :install
-end
-package "emacs" do
-  action :install
-end
+package "vim"
+package "emacs"
 
 # Terminal multiplexers
-package "screen" do
-  action :install
-end
-package "byobu" do
-  action :install
-end
+package "screen"
+package "byobu"
 
 # xubuntu uses this, even though it has "gnome" in the gname
-package "language-selector-gnome" do
-  action :install
-end
+package "language-selector-gnome"
 
 # part of xubuntu-desktop, needed for various parts of the OS to work
-package "libpam-ck-connector" do
-  action :install
-end
+package "libpam-ck-connector"
 execute "enable libpam-ck-connector" do
   command "sudo dpkg-reconfigure libpam-ck-connector"
   action :run
 end
 
 # The xubuntu display manager and greeter
-package "lightdm-gtk-greeter" do
-  action :install
-end
+package "lightdm-gtk-greeter"
 # Without this, lightdm-gtk-greeter will have missing icons
-package "xubuntu-artwork" do
-  action :install
-end
+package "xubuntu-artwork"
 
 # This must be run after lightdm-gtk-greeter is installed.
 include_recipe "duckduckhack-vm::artwork"
